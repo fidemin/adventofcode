80145a7ec01f8d998bbdc4cf47d464daf0ab9a40
Yunhong Min <yh.min@birdview.kr>
2020-12-06 11:09:18 +0900

    day02 part1 answer

--- day02/solution.py
+++ day02/solution.py
@@ -1,6 +1,15 @@
 from __future__ import annotations
 
 
+def parse_input_row(row: str):
+    """
+    :param row: e.g. 7-11 g: nnffggdmggr
+    :return:
+    """
+    rule_str, password = row.split(': ')
+    return rule_str, password
+
+
 class PasswordRule:
     def __init__(self, min_: int, max_: int, char: str):
         self._min = min_
@@ -33,3 +42,15 @@ class PasswordRule:
 
         if self._min <= match_count <= self._max:
             return True
+
+
+if __name__ == '__main__':
+    count = 0
+    with open('input.txt') as f:
+        for row in f:
+            rule_str, password = parse_input_row(row)
+            rule = PasswordRule.from_string(rule_str)
+            if rule.validate(password):
+                count += 1
+    print(count)
+
