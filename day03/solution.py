ea16edb726a02684c9db02aefa39ad5e1bf0f29e
Yunhong Min <yhmin84@gmail.com>
2020-12-06 21:35:19 +0900

    day03 part1 soultion

--- day03/solution.py
+++ day03/solution.py
@@ -1,3 +1,17 @@
 
 def tree_exists_at_position(row_pattern: str, pos: int):
     return True if row_pattern[pos] == '#' else False
+
+
+if __name__ == '__main__':
+    with open('input.txt', 'r') as f:
+        pos = 0
+        count = 0
+        for row_pattern in f:
+            row_pattern = row_pattern.strip()  # remove \n
+            result = tree_exists_at_position(row_pattern, pos)
+            if result:
+                count += 1
+            pos = (pos + 3) % len(row_pattern)
+
+        print(count)
