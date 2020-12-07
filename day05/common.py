8dfd6444f6c510f8c2d0ddffbb6a7ef15de9f1de
Yunhong Min <yhmin84@gmail.com>
2020-12-07 22:02:04 +0900

    day05 refactoring

---
+++ day05/common.py
@@ -0,0 +1,18 @@
+def binary_chars_to_int(left_char: str, right_char: str, chars: str):
+    min_value = 0
+    max_value = 2 ** len(chars) - 1
+    for char in chars:
+        if char == left_char:
+            max_value = (min_value + max_value) // 2
+        elif char == right_char:
+            min_value = (min_value + max_value) // 2 + 1
+        else:
+            assert False, f'{left_char}, {right_char} is only accepted. not {char}'
+    assert min_value == max_value
+    return min_value
+
+
+def calculate_seat_id(seat_chars: str):
+    row_num = binary_chars_to_int('F', 'B', seat_chars[:7])
+    col_num = binary_chars_to_int('L', 'R', seat_chars[7:])
+    return row_num * 8 + col_num
