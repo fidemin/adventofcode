db5dd5f5c17aeb923947b7914e1961ccb4d0cbb8
Yunhong Min <yhmin84@gmail.com>
2020-12-11 12:40:12 +0900

    day02 part2

--- day08/solution1.py
+++ day08/solution1.py
@@ -1,5 +1,6 @@
 
 from collections import namedtuple
+from copy import deepcopy
 
 Command = namedtuple('Command', ('op', 'arg'))
 
@@ -24,8 +25,11 @@ class Interpreter:
         self.accumulator = 0
 
     def execute(self):
-        while self._offset not in self._executed_offset_set:
+        while True:
             command = self._commands[self._offset]
+            if command.op == 'jmp' and self._offset in self._executed_offset_set:
+                return False
+
             self._executed_offset_set.add(self._offset)
             if command.op == 'nop':
                 self._offset = self._offset + 1
@@ -36,19 +40,24 @@ class Interpreter:
             elif command.op == 'jmp':
                 self._offset = self._offset + command.arg
 
+            if self._offset >= len(self._commands):
+                return True
+
 
 if __name__ == '__main__':
     with open('input.txt', 'r') as f:
-        commands = [CommandParser.parse(row) for row in f]
-        interpreter = Interpreter(commands)
-        interpreter.execute()
-        print(interpreter.accumulator)
+        this_commands = [CommandParser.parse(row) for row in f]
+        nop_or_jmp_offset_commands = [
+            (offset, command) for offset, command in enumerate(this_commands) if command.op in ['nop', 'jmp']]
 
+        for offset, nop_or_jmp_command in nop_or_jmp_offset_commands:
+            alt_commands = deepcopy(this_commands)
+            if nop_or_jmp_command.op == 'nop':
+                alt_commands[offset] = Command('jmp', nop_or_jmp_command.arg)
+            elif nop_or_jmp_command.op == 'jmp':
+                alt_commands[offset] = Command('nop', nop_or_jmp_command.arg)
 
-
-
-
-
-
-
-
+            interpreter = Interpreter(alt_commands)
+            result = interpreter.execute()
+            if result:
+                print(interpreter.accumulator)
