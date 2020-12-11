579672a2828b83471ac21a585dacb64adcae5646
Yunhong Min <yhmin84@gmail.com>
2020-12-11 18:20:30 +0900

    day09 part2

--- day09/solution.py
+++ day09/solution.py
@@ -2,12 +2,13 @@ from collections import defaultdict, deque
 
 
 if __name__ == '__main__':
+    target_file = 'input.txt'
     preamble_size = 25
     sum_list_by_number = defaultdict(list)
     sum_dict = defaultdict(int)
     count = 0
     result = None
-    with open('input.txt') as f:
+    with open(target_file) as f:
         initial_preamble = []
         for row in f:
             count += 1
@@ -42,7 +43,40 @@ if __name__ == '__main__':
                 sum_list_by_number[num].append(sum_value)
             preamble_deque.append(this_num)
 
-    print(result)
+    target_number = result
+    print(target_number)
+
+    with open(target_file) as f:
+        number_list = [int(row) for row in f]
+
+    left_cursor = 0
+    right_cursor = 0
+    is_right = True
+    sum_value = number_list[0]
+
+    min_number = None
+    max_number = None
+
+    while True:
+        if left_cursor == right_cursor:
+            right_cursor += 1
+            sum_value += number_list[right_cursor]
+        elif sum_value == target_number:
+            partial_number_list = number_list[left_cursor: right_cursor+1]
+            min_number = min(number_list[left_cursor: right_cursor+1])
+            max_number = max(number_list[left_cursor: right_cursor+1])
+            break
+        elif sum_value > target_number:
+            sum_value -= number_list[left_cursor]
+            left_cursor += 1
+        elif sum_value < target_number:
+            right_cursor += 1
+            sum_value += number_list[right_cursor]
+        else:
+            assert False
+
+    print(min_number, max_number)
+    print(min_number + max_number)
 
 
 
