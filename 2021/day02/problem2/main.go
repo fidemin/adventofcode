e0ef4f91ff93c471d2f39984af5d88919198b076
Yunhong Min <yunhongmin00@gmail.com>
2021-12-11 20:54:28 +0900

    day02 problem2

---
+++ 2021/day02/problem2/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Command struct {
+	direction string
+	value int
+}
+
+func parseInput(filename string) chan Command {
+	commandChan := make(chan Command)
+	go func() {
+		file, err := os.Open(filename)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer file.Close()
+
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			commandStr := scanner.Text()
+			commandSplit := strings.Split(commandStr, " ")
+
+			direction := commandSplit[0]
+			value, err := strconv.Atoi(commandSplit[1])
+			if err != nil {
+				log.Fatal(err)
+			}
+			commandChan <- Command{direction: direction, value: value}
+		}
+		close(commandChan)
+	}()
+	
+	return commandChan
+}
+
+func main() {
+	filename := os.Args[1]
+
+	position := 0
+	depth := 0
+	aim := 0
+
+	for command := range parseInput(filename) {
+		if command.direction == "forward" {
+			position += command.value
+			depth += aim * command.value
+		} else if command.direction == "down" {
+			aim += command.value
+		} else if command.direction == "up" {
+			aim -= command.value
+		}
+	}
+
+	fmt.Printf("current position: %d, depth: %d\n", position, depth)
+	fmt.Printf("result: %d\n", position * depth)
+}
