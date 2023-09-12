d89fdcd0195b8ba50661a26e11faedf6214f6813
Yunhong Min <yunhongmin00@gmail.com>
2023-09-12 13:24:47 +0900

    2022 p1 solved

--- 2022/day01/main.py
+++ 2022/day01/main.py
@@ -1,3 +1,21 @@
+import sys
+
+
+def parse_input_of_calories(filename):
+    with open(filename, 'r') as f:
+        calories_for_elfs = []
+        calories_for_elf = []
+        for row in f:
+            row = row.strip()
+            if len(row) == 0:
+                calories_for_elfs.append(calories_for_elf)
+                calories_for_elf = []
+            else:
+                calories_for_elf.append(int(row))
+
+        if len(calories_for_elf) != 0:
+            calories_for_elfs.append(calories_for_elf)
+        return calories_for_elfs
 
 
 def extract_max_calories(calories_for_elfs):
@@ -13,3 +31,21 @@ def extract_max_calories(calories_for_elfs):
     return current_max
 
 
+if __name__ == '__main__':
+    """
+    e.g.)
+    python main.py p1 input1.txt
+    python main.py p2 input2.txt
+    """
+    arguments = sys.argv
+    part = arguments[1]  # should be p1 or p2
+    input_file_name = arguments[2]
+
+    if part == 'p1':
+        calories = parse_input_of_calories(input_file_name)
+        print('Result')
+        print(extract_max_calories(calories))
+    elif part == 'p2':
+        raise NotImplementedError('p2 is not implemented yet')
+    else:
+        raise RuntimeError('{part} is not proper problem argument'.format(part=part))
--- 2022/day01/test_main.py
+++ 2022/day01/test_main.py
@@ -1,6 +1,25 @@
+import os
+
+from collections.abc import Iterable
 import pytest
 
 from . import main
+
+
+def assert_list(actual, expected):
+    assert len(actual) == len(expected)
+    for a, b in zip(actual, expected):
+        if isinstance(a, list) and isinstance(b, list):
+            assert_list(a, b)
+        else:
+            assert a == b
+
+
+def test_input_of_calories():
+    test_file_name = os.path.join(os.path.dirname(__file__), 'test.txt')
+    actual = main.parse_input_of_calories(test_file_name)
+    expected = [[100], [200, 300], [20, 30, 50]]
+    assert_list(actual, expected)
 
 
 @pytest.mark.parametrize(
