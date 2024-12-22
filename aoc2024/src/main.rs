41258abb0ef20c40dad775986131ed883be6986c
Yunhong Min <yunhongmin00@gmail.com>
2024-12-22 11:17:46 +0900

    day03 part1

--- aoc2024/src/main.rs
+++ aoc2024/src/main.rs
@@ -1,3 +1,18 @@
+
+mod day03;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let day_ = env::args().nth(1).expect("No day provided");
+    let part_ = env::args().nth(2).expect("No part provided");
+    let file_path = env::args().nth(3).expect("No file path provided");
+    if day_ == "day03" {
+        if part_ == "part1" {
+            day03::part1::solve(&file_path);
+        } else if part_ == "part2" {
+            // day03::part2::solve(&file_path);
+        } else {
+            println!("Invalid part provided");
+        }
+    }
 }
