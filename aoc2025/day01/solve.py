20bd8df369b42b16149b5efcb0471750e2be8daf
Yunhong Min <yunhongmin00@gmail.com>
2025-12-15 14:45:57 +0900

    2025 day01 part2

--- aoc2025/day01/solve.py
+++ aoc2025/day01/solve.py
@@ -17,6 +17,31 @@ def solve_part1(inputs):
             result += 1
     return result
 
+def solve_part2(inputs):
+    result = 0
+    position = 50
+
+    for input_ in inputs:
+        direction = input_[0]
+        value = int(input_[1:])
+
+        if direction == 'L':
+            # if start point is 0, action does not pass 0.
+            initial = 1 if position > 0 else 0
+            position -= value
+
+            if position <= 0:
+                result += initial + abs(position) // 100
+        else:
+            position += value
+
+            if position >= 100:
+                result += position // 100
+
+        position %= 100
+        # print(result, input_, position)
+    return result
+
 
 def solve(part, sample=False):
     if sample:
@@ -33,8 +58,5 @@ def solve(part, sample=False):
     if part == 'part1':
         result = solve_part1(inputs)
     else:
-        raise ValueError(f'Unknown part: {part}')
+        result = solve_part2(inputs)
     return result
-
-if __name__ == '__main__':
-    print(solve('part1', sample=False))
